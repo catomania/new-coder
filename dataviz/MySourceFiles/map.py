82cc694df6dd897a32312393819ea4067291a610
Catomania <morgan.v.zhou@gmail.com>
2015-08-27 21:19:21 -0700

    updating map.py

---
+++ dataviz/MySourceFiles/map.py
@@ -0,0 +1,12 @@
+""" Data Visualization Project
+Parse data from CSV file and make charts and plot on a map!
+
+Part 3: here we parse through each line and and create a geoson obj,
+
+to be collected into 1 geojson file for uploading into gist.github.com
+"""
+
+import geojson #ideally we could just import the dumps part
+
+import parse as p  #make it shorter!
+
