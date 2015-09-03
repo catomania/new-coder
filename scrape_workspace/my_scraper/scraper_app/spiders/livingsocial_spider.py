4840cdffdd3a26b9e47e2df5458ea65a0cd53164
Catomania <morgan.v.zhou@gmail.com>
2015-09-03 14:58:25 -0700

    adding spider

---
+++ scrape_workspace/my_scraper/scraper_app/spiders/livingsocial_spider.py
@@ -0,0 +1,12 @@
+#! -*- coding: utf-8 -*-
+
+"""
+Web Scraper Project
+
+Scrape data from a regularly updated website livingsocial.com and
+save to a database (postgres).
+
+Scrapy spider part - it actually performs scraping.
+"""
+
+#more about Scrappy: http://doc.scrapy.org/en/latest/intro/overview.html
